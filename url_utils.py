a08b4df79a939a9edde00ebd811b4f3ca6c79f1c
wasw100 <wasw100@gmail.com>
2013-08-28 14:21:39 +0800

    url_utils add test code

--- url_utils.py
+++ url_utils.py
@@ -9,3 +9,10 @@ def get_values_by_name(url, name):
     o = urlparse.urlparse(url)
     param_dict = urlparse.parse_qs(o.query)
     return param_dict.get(name)
+    
+    
+if __name__ == "__main__":
+    """test"""
+    url = "http://www.codeif.com?s=python"
+    print get_values_by_name(url, "s")
+    print get_values_by_name(url, "t")
